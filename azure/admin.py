4f10aa86ea6c6b454f1765c1cb00e1ec40cb3c95
Uchencho <aloziekelechi17@gmail.com>
2020-04-21 13:48:23 +0100

    export button added on admin

--- azure/admin.py
+++ azure/admin.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
+from import_export.admin import ImportExportModelAdmin
 
 # Register your models here.
 from .models import Predict
 
-class PredictAdmin(admin.ModelAdmin):
+@admin.register(Predict)
+class PredictAdmin(ImportExportModelAdmin):
     list_display = ["name","ticket_class", "sex", "age", "siblings", "parents", "probabiliy", "prediction"]
-
-admin.site.register(Predict, PredictAdmin)
